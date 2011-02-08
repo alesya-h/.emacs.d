5e0a1af53ebe92033b6b5411c1f51572b702ecf4
Ales Guzik <ales.guzik@gmail.com>
2011-02-08 15:44:58 +0200

    added starting rinari to rhtml hook

--- init.el
+++ init.el
@@ -158,7 +158,9 @@
 
 ;;; rhtml mode
 (add-to-list 'load-path "~/.emacs.d/plugins/rhtml")
-(require 'rhtml-mode)
+     (require 'rhtml-mode)
+     (add-hook 'rhtml-mode-hook
+     	  (lambda () (rinari-launch)))
 
 ;; rails-reloaded
 ;;(add-to-list 'load-path "~/.emacs.d/plugins/emacs-rails-reloaded")
