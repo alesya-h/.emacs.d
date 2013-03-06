6a27c475a778fb308bfeb9de923e03ec9fc630fa
Ales Guzik <me@aguzik.net>
2013-03-06 12:32:20 +0300

    250502 as default gpicker dir

--- init.el
+++ init.el
@@ -140,7 +140,7 @@
 (global-set-key (kbd "<f5>") 'recentf-open-files)
 
 (require 'gpicker)
-(setq *gpicker-project-dir* "/home/me/pmm/chef-repo")
+(setq *gpicker-project-dir* "/home/me/p/250502")
 (defun gpicker-set-root-and-find-file ()
   (interactive)
   (unless *gpicker-project-dir*
