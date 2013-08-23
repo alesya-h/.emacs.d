2796ed1d919972e02193c6ff3a9b9bf09109d56a
Ales Guzik <me@aguzik.net>
2013-08-23 11:35:13 +0300

    updated packages

--- elpa/tuareg-20130816.831/tuareg-pkg.el
+++
@@ -1,3 +0,0 @@
-(define-package "tuareg" "20130816.831" "OCaml mode for Emacs. [svn]"
-  '((caml "3.12.0.1")))
-
---
+++ elpa/tuareg-20130822.1015/tuareg-pkg.el
@@ -0,0 +1,3 @@
+(define-package "tuareg" "20130822.1015" "OCaml mode for Emacs. [svn]"
+  '((caml "3.12.0.1")))
+
