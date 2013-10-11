e168e6213a76a5a893eceb287bbe6a1a4406ce60
Ales Guzik <me@aguzik.net>
2013-10-11 10:20:54 +0300

    updated packages

--- elpa/gruber-darker-theme-20130922.1352/gruber-darker-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; gruber-darker-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "gruber-darker-theme" "gruber-darker-theme.el"
-;;;;;;  (21056 2669 783122 852000))
-;;; Generated autoloads from gruber-darker-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("gruber-darker-theme-pkg.el") (21056 2669
-;;;;;;  860343 102000))
-
-;;;***
-
-(provide 'gruber-darker-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; gruber-darker-theme-autoloads.el ends here
--- elpa/zenburn-theme-20130914.1044/zenburn-theme-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; zenburn-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "zenburn-theme" "zenburn-theme.el" (21049 28616
-;;;;;;  869858 879000))
-;;; Generated autoloads from zenburn-theme.el
-
-(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-(add-to-list 'safe-local-eval-forms '(when (require 'rainbow-mode nil t) (rainbow-mode 1)))
-
-;;;***
-
-;;;### (autoloads nil nil ("zenburn-theme-pkg.el") (21049 28616 974228
-;;;;;;  24000))
-
-;;;***
-
-(provide 'zenburn-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; zenburn-theme-autoloads.el ends here
